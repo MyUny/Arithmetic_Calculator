4961d3f5846c50ea72e45c9e2d90a2345cbc83d9
MyUny <german.caggianese@pm.me>
2023-06-06 23:06:35 -0300

    Feature: justParentheses

--- Calculator.java
+++ Calculator.java
@@ -26,6 +26,20 @@ public class Calculator {
         return true;
     }
 
+    public String justParentheses(String arithInput){
+        String aux = arithInput ;
+        for(int i=0; i<aux.length(); i++){
+            if (aux.charAt(i) != 41 && aux.charAt(i) != 40){
+               aux = aux.replace(aux.charAt(i),(char)55);
+            }
+
+        }
+        return aux.replaceAll("7","");
+    }
+
+    public boolean parenthesesChecker(String parenInput){
+        return true;
+    }
 
 
 
--- Main.java
+++ Main.java
@@ -2,8 +2,10 @@ public class Main {
     public static void main(String[] args) {
         boolean menu = true;
         Calculator c = new Calculator();
-        String a = "!a  t  !2  3  s";
+        String a = ")!a(((  t  !2  )3  s";
         a = a.replaceAll("\\s","");
+        System.out.println(c.justParentheses(a));
+
         if(c.notValid(a)){
             //lógica
         }else{
