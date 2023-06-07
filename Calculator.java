192d90fd37ca75dfcc4066a89e3335b7771e8d51
MyUny <german.caggianese@pm.me>
2023-06-06 22:35:07 -0300

    Feature: delete blank spaces in input

--- Calculator.java
+++ Calculator.java
@@ -26,4 +26,7 @@ public class Calculator {
         return true;
     }
 
+
+
+
 }
--- Main.java
+++ Main.java
@@ -2,15 +2,13 @@ public class Main {
     public static void main(String[] args) {
         boolean menu = true;
         Calculator c = new Calculator();
-        String a = "!at!23s";
+        String a = "!a  t  !2  3  s";
+        a = a.replaceAll("\\s","");
         if(c.notValid(a)){
             //lógica
         }else{
             menu = false;
         }
-
-
-
 
     }
 }
